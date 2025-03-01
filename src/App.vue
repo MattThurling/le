841e1f1bba0677d768a2ff1f9aeee7e0c32111b3
Matt Thurling <mthurling@icloud.com>
2025-03-01 17:43:01 +0000

    API

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <template>
   <h1>Large English</h1>
   <p>Coming soon: a new resource site for English language learners, featuring serious exam preparation drills and daft videos!</p>
+  <p>{{ message }}</p>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      message: "Loading..."
+    };
+  },
+  async mounted() {
+    try {
+      const response = await axios.get("https://le-api.onrender.com/api/message/");
+      console.log(response.data.message)
+      this.message = response.data.message;
+    } catch (error) {
+      console.error("API call failed:", error);
+      this.message = "Error fetching message";
+    }
+  }
+};
+</script>
 
