d3e90afc61ecbbc142ee37502a325315d435b55f
Matt Thurling <mthurling@icloud.com>
2025-03-20 18:02:20 +0000

    level

--- frontend/src/StylerForDjango.vue
+++ frontend/src/StylerForDjango.vue
@@ -5,7 +5,7 @@
             BASE
         </div>
 
-        <div class="text-center text-base-content divider mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 card card-sm border border-gray-700 w-full h-40 object-cover rounded-t-lg card-body card-title text-lg font-semibold flex items-center gap-3 mt-3 btn btn-primary cursor-pointer">
+        <div class="text-center text-base-content divider mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 card card-sm border border-gray-700 w-full h-40 object-cover rounded-t-lg card-body card-title text-lg font-semibold flex items-center gap-3 mt-3 btn btn-primary cursor-pointer relative absolute top-2 right-2 badge badge-primary badge-outline badge-info badge-soft badge-secondary">
             PROMPTS
         </div>
 
