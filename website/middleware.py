248aa1c6ef3cba925cb65300903169b2cf5380a5
whiskyplausible <8440108+whiskyplausible@users.noreply.github.com>
2025-05-08 16:54:52 +0200

    bak

--- website/middleware.py
+++ website/middleware.py
@@ -1,26 +1,12 @@
 from django.utils.deprecation import MiddlewareMixin
 from .models import Organisation
-from django.conf import settings
 
 class OrganisationMiddleware(MiddlewareMixin):
     def process_request(self, request):
         host = request.get_host().split(':')[0]
-        base_domain = settings.BASE_DOMAIN
-        domain_parts = host.split('.')
-        base_parts = base_domain.split('.')
-
-        # Check if host matches the base domain exactly
-        if host == base_domain:
-            # root
-            request.organisation = None
-            return
-
-        # Check if it's a subdomain of the base domain
-        if len(domain_parts) > len(base_parts) and domain_parts[-len(base_parts):] == base_parts:
-            subdomain = '.'.join(domain_parts[:-len(base_parts)])
-            try:
-                request.organisation = Organisation.objects.get(subdomain=subdomain)
-            except Organisation.DoesNotExist:
-                request.organisation = None
-            return
+        print(host)
+        try:
+            request.organisation = Organisation.objects.get(domain=host)
+        except Organisation.DoesNotExist:
+            request.organisation = None 
 
